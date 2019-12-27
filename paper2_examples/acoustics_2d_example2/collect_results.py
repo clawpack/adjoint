fef44bcc47dcc9ba7b4903d5c5d741ec95dd89b0
Randy LeVeque <rjl@uw.edu>
2019-12-27 12:40:49 -0800

    scripts for plots

---
+++ paper2_examples/acoustics_2d_example2/collect_results.py
@@ -0,0 +1,80 @@
+
+from pylab import *
+import clawpack.pyclaw.gauges as gauges
+from scipy.interpolate import interp1d
+
+gaugeno=0
+t1 = 20.5
+t2 = 21.5
+
+truns = ['1600x1200','0025','005','01','02','lte0025','lte005','lte01','lte02']
+outdirs = ['_output_%s' % trun for trun in truns]
+
+tunif = linspace(t1,t2,10000)
+
+punif = {}
+perr = {}
+cpu = {}
+
+for k in range(len(truns)):
+    outdir = outdirs[k]
+    trun = truns[k]
+    
+    timings = open('%s/timing.txt' % outdir).readlines()
+    for line in timings:
+        if 'Total time:' in line:
+            tokens = line.split()
+            cpu[k] = float(tokens[-1])
+            print('%s: CPU = %s' % (trun,tokens[-1]))
+    gauge = gauges.GaugeSolution(gaugeno, outdir)
+    t = gauge.t
+    q = gauge.q
+    p = q[0,:]
+    j = where(logical_and(t>t1-0.1,t<t2+0.1))[0]
+    tt = t[j]
+    pp = p[j]
+    
+    pfcn = interp1d(tt,pp)
+    punif[trun] = pfcn(tunif)
+    if k>0:
+        pfine = punif[truns[0]]
+        perr[trun] = abs(punif[trun] - pfine).max()
+        print('%s: diff = %.4f' % (trun,perr[trun]))
+    
+tol = [.0025,.005,.01,.02]
+cpu_lte = [1329,581,225,63]
+cpu_a = [407,125,17,2]
+err_a = [0.0027,.0083,.0354,.0837]
+err_lte = [.0031,.0067,.0122,.0347]
+cpu_fine = 2823.5
+
+figure(21)
+clf()
+loglog(tol,err_lte,'bo-',label='forward error flagging') 
+loglog(tol,err_a,'rs-',label='adjoint error flagging')    
+axis('scaled'); grid(True)
+axis([1e-3,1e-1,1e-3,1e-1])
+loglog([1e-3,0.1],[1e-3,0.1],'k--')
+xlabel('tolerance specified')
+ylabel('max diff in p over [t1,t2]')
+legend(loc='lower right',framealpha=1)
+if 1:
+    fname = 'error_plot.png'
+    savefig(fname, bbox_inches='tight')
+    print('Created ',fname)
+
+figure(22, figsize=(8,5))
+clf()
+semilogx(tol,cpu_lte,'bo-',label='forward error flagging')
+semilogx(tol,cpu_a,'rs-',label='adjoint error flagging')
+semilogx(tol,cpu_fine*ones(len(tol)),'k--')
+axis([1e-3,0.1,0,3000])
+text(tol[0],cpu_fine+20, 'uniform fine grid')
+grid(True)
+xlabel('tolerance specified')
+ylabel('CPU time (seconds)')
+legend(loc='lower right',framealpha=1)
+if 1:
+    fname = 'cpu_plot.png'
+    savefig(fname, bbox_inches='tight')
+    print('Created ',fname)
